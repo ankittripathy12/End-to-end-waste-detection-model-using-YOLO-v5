f846a15351e314048df429aa067ba8a8c82826a8
ankittripathy12 <ankittriapthy372@gmail.com>
2026-07-04 18:05:40 +0530

    project structure,requirements.txt and notebook experiment added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Ankit Kumar Tripathy',
+    author_email= 'ankittripathy372@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
